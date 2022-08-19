fe205172cf59a3d893c813dd2c63aa47c962e377
Виктор Безунов <victorbezunov@gmail.com>
2022-08-19 22:45:30 +0300

    add sing in to first load page

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,12 @@ import { Vue } from "vue-class-component";
 export default class AppComponent extends Vue {
   created() {
     this.$api.AuthService.getUserFromCookie();
+    if (this.$api.AuthService.IsLogin) {
+      this.$api.AuthService.SingIn({
+        userName: this.$api.AuthService.User.userName,
+        password: this.$api.AuthService.User.password,
+      });
+    }
   }
 }
 </script>
